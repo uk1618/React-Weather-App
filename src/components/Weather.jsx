b84dad1d710007111148bdf02a297e8e0416dcf7
Umut Konak <ukonak360@yandex.com>
2024-09-27 22:52:23 +0300

    Fetch and display weather data dynamically (Openweather API)

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -9,6 +9,51 @@ import rain_icon from '../assets/rain.png'
 import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 const Weather = () => {
+
+    const [weatherData, setWeatherData] = useState(false);
+
+   
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13n": snow_icon,
+        "13d": snow_icon,
+    }
+   
+    const search = async (city) => {
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`                
+        const response = await fetch(url);
+        const data = await response.json();
+        const icon = allIcons[data.weather[0].icon]
+        return setWeatherData({
+            humidity: data.main.humidity,
+            windSpeed: data.wind.speed,
+            temperature: Math.floor(data.main.temp),
+            location: data.name,
+            icon: icon
+        })
+    } 
+    catch (error) {
+        console.log("error",error);
+        
+    }
+   }
+
+   useEffect(()=> {
+    search("london")
+   }, [])
+
   return (
     <div className='weather'>
         
@@ -18,21 +63,21 @@ const Weather = () => {
         <img src={search_icon} />
       </div>
         {/*----- Weather Details -----*/}
-        <img className='weather-icon' src={clear_icon} />
-        <p className='temperature'>16°c</p>
-        <p className='location'>London</p>
+        <img className='weather-icon' src={weatherData.icon} />
+        <p className='temperature'>{weatherData.temperature}°c</p>
+        <p className='location'>{weatherData.location}</p>
         <div className='weather-data'>
            <div className="col">
             <img src={humidity_icon}/>
             <div>
-                <p>91 %</p>
+                <p>{weatherData.humidity} %</p>
                 <span>Humidity</span>
             </div>
            </div>
            <div className="col">
             <img src={wind_icon}/>
             <div>
-                <p>4.2 Km/h</p>
+                <p>{weatherData.windSpeed} Km/h</p>
                 <span>Wind Speed</span>
             </div>
            </div>
